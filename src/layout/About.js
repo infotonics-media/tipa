b42e5d1be78e58232be5b84283f3bf1db7b80e64
infotonics-media <55031318+infotonics-media@users.noreply.github.com>
2023-12-18 15:43:35 +0530

    Update About.js

--- src/layout/About.js
+++ src/layout/About.js
@@ -38,7 +38,10 @@ const About = () => {
         {/* <div className="backgrnd"></div> */}
         <div className="aboutTitle">
 
-          <h2>12<sup>th</sup> January, 2024.</h2>
+          <h2>
+              12<sup>th</sup>
+              January, 2024.
+          </h2>
         </div>
         <div className="video">
 
